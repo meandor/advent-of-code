3168ea565662faac7f8bd1a6dc865e8decd5757b
Daniel Schruhl <danielschruhl@gmail.com>
2020-12-03 06:09:21 +0100

    Add day 1.1 solution

--- day1.pl
+++ day1.pl
@@ -2,19 +2,34 @@ read_input(File, Input) :-
     read_file_to_string(File, RawInput, []),
     split_string(RawInput, "\n", "\n", Input).
 
-sum_is_2020(A, B) :-
+sum_is_2020(A, B, C) :-
     number_string(ANumber, A),
     number_string(BNumber, B),
-    (2020 is (ANumber + BNumber)).
+    number_string(CNumber, C),
+    (2020 is (ANumber + BNumber + CNumber)).
 
 find_2020_pairs(Values, X, Y) :-
     member(X, Values),
     member(Y, Values),
-    sum_is_2020(X, Y).
+    sum_is_2020(X, Y, "0").
 
-pairs_of_2020(File, XNumber, YNumber, Z) :-
+find_2020_triplet(Values, X, Y, Z) :-
+    member(X, Values),
+    member(Y, Values),
+    member(Z, Values),
+    sum_is_2020(X, Y, Z).
+
+pairs_of_2020(File, XNumber, YNumber, Multiplication) :-
     read_input(File, Input),
     find_2020_pairs(Input, X, Y),
     number_string(XNumber, X),
     number_string(YNumber, Y),
-    (Z is (XNumber * YNumber)).
+    (Multiplication is (XNumber * YNumber)).
+
+pairs_of_2020(File, XNumber, YNumber, ZNumber, Multiplication) :-
+    read_input(File, Input),
+    find_2020_triplet(Input, X, Y, Z),
+    number_string(XNumber, X),
+    number_string(YNumber, Y),
+    number_string(ZNumber, Z),
+    (Multiplication is (XNumber * YNumber * ZNumber)).
--- main.pl
+++ main.pl
@@ -5,12 +5,24 @@
 :- initialization main.
 
 day1_solution :-
-    pairs_of_2020("resources/day1.txt", X, Y, Z),
-    write("Day 1 solution - Number 1: "),
+    pairs_of_2020("resources/day1.txt", X, Y, Multiplied),
+    write("Day 1.1 solution - Number 1: "),
     write(X),
     write(", Number 2: "),
     write(Y),
     write(", Multiplied: "),
-    write(Z).
+    write(Multiplied),
+    write("\n").
 
-main :- day1_solution.
+day1_1_solution :-
+    pairs_of_2020("resources/day1.txt", X, Y, Z, Multiplied),
+    write("Day 1.2 solution - Number 1: "),
+    write(X),
+    write(", Number 2: "),
+    write(Y),
+    write(", Number 3: "),
+    write(Z),
+    write(", Multiplied: "),
+    write(Multiplied).
+
+main :- day1_solution, day1_1_solution.
