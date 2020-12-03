59ee77e1abea9ea3fe7cabdcb21ea7e646c348f7
Daniel Schruhl <danielschruhl@gmail.com>
2020-12-03 06:16:33 +0100

    Add day2 skeleton

---
+++ day2/project.clj
@@ -0,0 +1,9 @@
+(defproject day2 "0.1.0-SNAPSHOT"
+  :description "solution for day 2 advent of code"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :main ^:skip-aot day2.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
