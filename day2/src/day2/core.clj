56a3629b23cb379bb5cfc04773f50e77fe2ddf4c
Daniel Schruhl <danielschruhl@gmail.com>
2020-12-03 08:23:53 +0100

    Add solution for day 2.2

--- day2/src/day2/core.clj
+++ day2/src/day2/core.clj
@@ -5,22 +5,34 @@
 (defn parse-line [line]
   (let [[_ min-occurrence max-occurrence letter password]
         (re-matches #"(\d+)-(\d+) (\w+): (\w+)" line)]
-    {:password       password
-     :min-occurrence (read-string min-occurrence)
-     :max-occurrence (read-string max-occurrence)
-     :letter         letter}))
+    {:password     password
+     :first-index  (read-string min-occurrence)
+     :second-index (read-string max-occurrence)
+     :letter       letter}))
 
 (defn count-letter-occurrence [letter word]
-  (->> (seq word)
+  (->> word
        (map #(str %))
        (filter #(= letter %))
        (count)))
 
-(defn valid-sled-password? [{:keys [password min-occurrence max-occurrence letter]}]
+(defn valid-sled-password? [{:keys [password first-index second-index letter]}]
   (<=
-    min-occurrence
+    first-index
     (count-letter-occurrence letter password)
-    max-occurrence))
+    second-index))
+
+(defn- letter-at-index [index password-sequence]
+  (nth password-sequence (dec index)))
+
+(defn- xor [A B]
+  (or (and A (not B))
+      (and B (not A))))
+
+(defn valid-toboggan-password? [{:keys [password first-index second-index letter]}]
+  (let [password-sequence (map #(str %) password)]
+    (xor (= letter (letter-at-index first-index password-sequence))
+         (= letter (letter-at-index second-index password-sequence)))))
 
 (defn count-valid-passwords [password-policy-fn passwords-with-rules]
   (count (filter password-policy-fn passwords-with-rules)))
@@ -38,5 +50,5 @@
       (->> (line-seq rdr)
            (map parse-line)
            (print-valid-passwords "sled" valid-sled-password?)
-           ))
+           (print-valid-passwords "toboggan" valid-toboggan-password?)))
     (println "usage: java -jar day2.jar [input.txt-location]")))
--- day2/test/day2/core_test.clj
+++ day2/test/day2/core_test.clj
@@ -4,17 +4,17 @@
 
 (deftest should-parse-line
   (testing "same number of min max occurrence"
-    (is (= {:password       "foo"
-            :min-occurrence 1
-            :max-occurrence 1
-            :letter         "f"}
+    (is (= {:password     "foo"
+            :first-index  1
+            :second-index 1
+            :letter       "f"}
            (core/parse-line "1-1 f: foo"))))
 
   (testing "different number of min max occurrence"
-    (is (= {:password       "foo"
-            :min-occurrence 1
-            :max-occurrence 3
-            :letter         "o"}
+    (is (= {:password     "foo"
+            :first-index  1
+            :second-index 3
+            :letter       "o"}
            (core/parse-line "1-3 o: foo")))))
 
 (deftest should-count-letters
@@ -27,28 +27,59 @@
   (testing "n occurrence"
     (is (= 3 (core/count-letter-occurrence "b" "foobbb")))))
 
-(deftest should-check-is-valid-password?
+(deftest should-check-is-valid-sled-password?
   (with-redefs [core/count-letter-occurrence (constantly 1)]
     (testing "valid password with min=occurrence=max"
-      (is (= true (core/valid-sled-password? {:password  "foo"
-                                         :min-occurrence 1
-                                         :max-occurrence 1
-                                         :letter         "f"}))))
+      (is (= true (core/valid-sled-password? {:password     "foo"
+                                              :first-index  1
+                                              :second-index 1
+                                              :letter       "f"}))))
 
     (testing "valid password with min<=occurrence<max"
-      (is (= true (core/valid-sled-password? {:password  "foo"
-                                         :min-occurrence 1
-                                         :max-occurrence 2
-                                         :letter         "f"})))))
+      (is (= true (core/valid-sled-password? {:password     "foo"
+                                              :first-index  1
+                                              :second-index 2
+                                              :letter       "f"})))))
 
   (with-redefs [core/count-letter-occurrence (constantly 2)]
     (testing "valid password with min<occurrence<=max"
-      (is (= true (core/valid-sled-password? {:password  "foo"
-                                         :min-occurrence 1
-                                         :max-occurrence 2
-                                         :letter         "o"}))))
+      (is (= true (core/valid-sled-password? {:password     "foo"
+                                              :first-index  1
+                                              :second-index 2
+                                              :letter       "o"}))))
     (testing "invalid password"
-      (is (= false (core/valid-sled-password? {:password  "foo"
-                                          :min-occurrence 3
-                                          :max-occurrence 4
-                                          :letter         "o"}))))))
+      (is (= false (core/valid-sled-password? {:password     "foo"
+                                               :first-index  3
+                                               :second-index 4
+                                               :letter       "o"}))))))
+
+(deftest should-check-is-valid-toboggan-password?
+  (testing "false password with same index"
+    (is (= false (core/valid-toboggan-password? {:password     "fob"
+                                                :first-index  2
+                                                :second-index 2
+                                                :letter       "o"}))))
+
+  (testing "valid password with only first index"
+    (is (= true (core/valid-toboggan-password? {:password     "fob"
+                                                :first-index  1
+                                                :second-index 2
+                                                :letter       "f"}))))
+
+  (testing "valid password with only second index"
+    (is (= true (core/valid-toboggan-password? {:password     "fob"
+                                                :first-index  1
+                                                :second-index 2
+                                                :letter       "o"}))))
+
+  (testing "invalid password both indices are available"
+    (is (= false (core/valid-toboggan-password? {:password     "foo"
+                                                :first-index  2
+                                                :second-index 3
+                                                :letter       "o"}))))
+
+  (testing "invalid password does not contain letter"
+    (is (= false (core/valid-toboggan-password? {:password     "foo"
+                                                :first-index  1
+                                                :second-index 3
+                                                :letter       "k"})))))
