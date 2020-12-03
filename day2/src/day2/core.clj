1651341b1baed2e5407b7e521644ee36242f166a
Daniel Schruhl <danielschruhl@gmail.com>
2020-12-03 06:50:27 +0100

    Add parsing

--- day2/src/day2/core.clj
+++ day2/src/day2/core.clj
@@ -5,3 +5,11 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn parse-line [line]
+  (let [[_ min-occurrence max-occurrence letter password]
+        (re-matches #"(\d+)-(\d+) (\w+): (\w+)" line)]
+    {:password       password
+     :min-occurrence (read-string min-occurrence)
+     :max-occurrence (read-string max-occurrence)
+     :letter         letter}))
--- day2/test/day2/core_test.clj
+++ day2/test/day2/core_test.clj
@@ -1,7 +1,18 @@
 (ns day2.core-test
   (:require [clojure.test :refer :all]
-            [day2.core :refer :all]))
+            [day2.core :as core]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest should-parse-line
+  (testing "same number of min max occurrence"
+    (is (= {:password       "foo"
+            :min-occurrence 1
+            :max-occurrence 1
+            :letter         "f"}
+           (core/parse-line "1-1 f: foo"))))
+
+  (testing "different number of min max occurrence"
+    (is (= {:password       "foo"
+            :min-occurrence 1
+            :max-occurrence 3
+            :letter         "o"}
+           (core/parse-line "1-3 o: foo")))))
