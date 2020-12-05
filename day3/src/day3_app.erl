b9dc20b5d9bce768800d2d55a84f9707a2faded0
Daniel Schruhl <danielschruhl@gmail.com>
2020-12-05 08:35:07 +0100

    Refactor

--- day3/src/day3_app.erl
+++ day3/src/day3_app.erl
@@ -1,8 +1,3 @@
-%%%-------------------------------------------------------------------
-%% @doc day3 public API
-%% @end
-%%%-------------------------------------------------------------------
-
 -module(day3_app).
 
 -behaviour(application).
@@ -10,9 +5,7 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
-    day3_sup:start_link().
+  day3_sup:start_link().
 
 stop(_State) ->
-    ok.
-
-%% internal functions
+  ok.
--- day3/src/day3_sup.erl
+++ day3/src/day3_sup.erl
@@ -1,8 +1,3 @@
-%%%-------------------------------------------------------------------
-%% @doc day3 top level supervisor.
-%% @end
-%%%-------------------------------------------------------------------
-
 -module(day3_sup).
 
 -behaviour(supervisor).
@@ -14,22 +9,11 @@
 -define(SERVER, ?MODULE).
 
 start_link() ->
-    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+  supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
-%% sup_flags() = #{strategy => strategy(),         % optional
-%%                 intensity => non_neg_integer(), % optional
-%%                 period => pos_integer()}        % optional
-%% child_spec() = #{id => child_id(),       % mandatory
-%%                  start => mfargs(),      % mandatory
-%%                  restart => restart(),   % optional
-%%                  shutdown => shutdown(), % optional
-%%                  type => worker(),       % optional
-%%                  modules => modules()}   % optional
 init([]) ->
-    SupFlags = #{strategy => one_for_all,
-                 intensity => 0,
-                 period => 1},
-    ChildSpecs = [],
-    {ok, {SupFlags, ChildSpecs}}.
-
-%% internal functions
+  SupFlags = #{strategy => one_for_one,
+    intensity => 2,
+    period => 5},
+  ChildSpecs = [],
+  {ok, {SupFlags, ChildSpecs}}.
