4270bc82a6a490a7d2b9da7a86bb39310d96978d
Daniel Schruhl <danielschruhl@gmail.com>
2020-12-06 10:20:03 +0100

    Fix scapegoat

--- day4/src/main/scala/com/github/meandor/Parser.scala
+++ day4/src/main/scala/com/github/meandor/Parser.scala
@@ -8,7 +8,7 @@ object Parser {
   def parse(line: String): Map[String, String] = {
     val pattern                    = """((\w+):(\S+))+""".r
     val matches                    = (pattern findAllIn line).toSeq
-    val value: Map[String, String] = Map()
+    val value: Map[String, String] = Map.empty
     matches.foldLeft(value) { (acc, element) =>
       val pair = element.split(":")
       acc + (pair(0) -> pair(1))
@@ -16,10 +16,10 @@ object Parser {
   }
 
   def parse(lines: Iterator[String]): Seq[Passport] = {
-    val value: Seq[Passport] = Seq(Passport(Map()))
+    val value: Seq[Passport] = Seq(Passport(Map.empty))
     lines.foldLeft(value) { (acc, line) =>
       if (line == "") {
-        acc.+:(Passport(Map()))
+        acc.+:(Passport(Map.empty))
       } else {
         val newValues             = merge(acc.head.values, parse(line))
         val passportWithNewValues = Passport(newValues)
